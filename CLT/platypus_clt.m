e5ab8755114a642eb31a40418656de0c5071eb20
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2026-03-25 20:52:02 +0000

    Fixed several minor issues in command line tool

--- CLT/platypus_clt.m
+++ CLT/platypus_clt.m
@@ -38,7 +38,6 @@
 #import <limits.h>
 #import <string.h>
 #import <fcntl.h>
-#import <errno.h>
 #import <getopt.h>
 
 #import "Common.h"
@@ -186,6 +185,8 @@ int main(int argc, const char *argv[]) {
                     // Add to bundled files array in spec
                     if (properties[AppSpecKey_BundledFiles] == nil) {
                         properties[AppSpecKey_BundledFiles] = [NSMutableArray array];
+                    } else if ([properties[AppSpecKey_BundledFiles] isKindOfClass:[NSMutableArray class]] == NO) {
+                        properties[AppSpecKey_BundledFiles] = [properties[AppSpecKey_BundledFiles] mutableCopy];
                     }
                     [properties[AppSpecKey_BundledFiles] addObject:fp];
                 }
@@ -258,7 +259,7 @@ int main(int argc, const char *argv[]) {
                 NSString *iconPath = @(optarg);
                 
                 // empty icon path means just default app icon, otherwise a path to an icns file
-                if (iconPath && [iconPath isEqualTo:@""] == NO) {
+                if (iconPath && [iconPath isEqualToString:@""] == NO) {
                     iconPath = [MakeAbsolutePath(iconPath) stringByResolvingSymlinksInPath];
                     // if we have proper arg, make sure file exists
                     if ([fm fileExistsAtPath:iconPath] == NO) {
@@ -282,7 +283,7 @@ int main(int argc, const char *argv[]) {
                 NSString *iconPath = @(optarg);
                 
                 // empty icon path means just default app icon, otherwise a path to an icns file
-                if (![iconPath isEqualTo:@""]) {
+                if (![iconPath isEqualToString:@""]) {
                     iconPath = [MakeAbsolutePath(iconPath) stringByResolvingSymlinksInPath];
                     // if we have proper arg, make sure file exists
                     if (![fm fileExistsAtPath:iconPath]) {
@@ -452,7 +453,7 @@ int main(int argc, const char *argv[]) {
             case 'Y':
             {
                 NSString *title = @(optarg);
-                if ([title isEqualToString:@""] || title == nil) {
+                if (title == nil || [title isEqualToString:@""]) {
                     NSPrintErr(@"Error: Empty status item title.");
                     exit(EXIT_FAILURE);
                 }
@@ -524,10 +525,15 @@ int main(int argc, const char *argv[]) {
             
             // Print help with list of options
             case 'h':
-            default:
             {
                 PrintHelp();
                 exit(EXIT_SUCCESS);
+            }
+                break;
+            default:
+            {
+                PrintHelp();
+                exit(EXIT_FAILURE);
             }
                 break;
         }
@@ -577,7 +583,11 @@ int main(int argc, const char *argv[]) {
         appSpec = [PlatypusAppSpec specWithDefaults];
         [appSpec addEntriesFromDictionary:properties];
         
-        printStdout ? [appSpec dump] : [appSpec writeToFile:destPath];
+        if (printStdout) {
+            [appSpec dump];
+        } else {
+            [appSpec writeToFile:destPath];
+        }
         
         exit(EXIT_SUCCESS);
     }
@@ -690,7 +700,7 @@ static NSString *ReadStandardInputToFile(void) {
 static NSString *MakeAbsolutePath(NSString *path) {
     NSString *absPath = [path stringByExpandingTildeInPath];
     if ([absPath isAbsolutePath] == NO) {
-        absPath = [[FILEMGR currentDirectoryPath] stringByAppendingPathComponent:path];
+        absPath = [[FILEMGR currentDirectoryPath] stringByAppendingPathComponent:absPath];
     }
     return [absPath stringByStandardizingPath];
 }
@@ -723,52 +733,53 @@ platypus [OPTIONS] scriptPath [destinationPath]\n\
 \n\
 Options:\n\
 \n\
-    -O --generate-profile              Generate a profile instead of an app\n\
+    -O --generate-profile                 Generate a profile instead of an app\n\
 \n\
-    -P --load-profile [profilePath]    Load settings from profile document\n\
-    -a --name [name]                   Set name of application bundle\n\
-    -o --interface-type [type]         Set interface type. See man page for accepted types\n\
-    -p --interpreter [interpreterPath] Set interpreter for script\n\
+    -P --load-profile [profilePath]       Load settings from profile document\n\
+    -a --name [name]                      Set name of application bundle\n\
+    -o --interface-type [type]            Set interface type. See man page for accepted types\n\
+    -p --interpreter [interpreterPath]    Set interpreter for script\n\
 \n\
-    -i --app-icon [iconPath]           Set icon for application\n\
-    -u --author [author]               Set name of application author\n\
-    -Q --document-icon [iconPath]      Set icon for documents\n\
-    -V --app-version [version]         Set version of application\n\
-    -I --bundle-identifier [idstr]     Set bundle identifier (e.g. org.yourname.appname)\n\
+    -i --app-icon [iconPath]              Set icon for application\n\
+    -u --author [author]                  Set name of application author\n\
+    -Q --document-icon [iconPath]         Set icon for documents\n\
+    -V --app-version [version]            Set version of application\n\
+    -I --bundle-identifier [idstr]        Set bundle identifier (e.g. org.yourname.appname)\n\
 \n\
-    -A --admin-privileges              App runs with Administrator privileges\n\
-    -D --droppable                     App accepts dropped files as arguments to script\n\
-    -F --text-droppable                App accepts dropped text passed to script via STDIN\n\
-    -Z --file-prompt                   App presents an open file dialog when launched\n\
-    -N --service                       App registers as a Mac OS X Service\n\
-    -B --background                    App runs in background (LSUIElement)\n\
-    -R --quit-after-execution          App quits after executing script\n\
+    -A --admin-privileges                 App runs with Administrator privileges\n\
+    -D --droppable                        App accepts dropped files as arguments to script\n\
+    -F --text-droppable                   App accepts dropped text passed to script via STDIN\n\
+    -Z --file-prompt                      App presents an open file dialog when launched\n\
+    -N --service                          App registers as a Mac OS X Service\n\
+    -B --background                       App runs in background (LSUIElement)\n\
+    -R --quit-after-execution             App quits after executing script\n\
+    -W --notifications                    App requests permission to send notifications\n\
 \n\
-    -b --text-background-color [color] Set background color of text view (e.g. '#ffffff')\n\
-    -g --text-foreground-color [color] Set foreground color of text view (e.g. '#000000')\n\
-    -n --text-font [fontName]          Set font for text view (e.g. 'Monaco 10')\n\
-    -X --suffixes [suffixes]           Set suffixes handled by app, separated by |\n\
-    -T --uniform-type-identifiers      Set uniform type identifiers handled by app, separated by |\n\
-    -U --uri-schemes                   Set URI schemes handled by app, separated by |\n\
-    -G --interpreter-args [arguments]  Set arguments for script interpreter, separated by |\n\
-    -C --script-args [arguments]       Set arguments for script, separated by |\n\
+    -b --text-background-color [color]    Set background color of text view (e.g. '#ffffff')\n\
+    -g --text-foreground-color [color]    Set foreground color of text view (e.g. '#000000')\n\
+    -n --text-font [fontName]             Set font for text view (e.g. 'Monaco 10')\n\
+    -X --suffixes [suffixes]              Set suffixes handled by app, separated by |\n\
+    -T --uniform-type-identifiers [utis]  Set uniform type identifiers handled by app, separated by |\n\
+    -U --uri-schemes [schemes]            Set URI schemes handled by app, separated by |\n\
+    -G --interpreter-args [arguments]     Set arguments for script interpreter, separated by |\n\
+    -C --script-args [arguments]          Set arguments for script, separated by |\n\
 \n\
-    -K --status-item-kind [kind]       Set Status Item kind ('Icon' or 'Text')\n\
-    -Y --status-item-title [title]     Set title of Status Item\n\
-    -L --status-item-icon [imagePath]  Set icon of Status Item\n\
-    -c --status-item-sysfont           Status Item should use the system font for menu item text\n\
-    -q --status-item-template-icon     Status Item icon should be treated as a template by AppKit\n\
+    -K --status-item-kind [kind]          Set Status Item kind ('Icon' or 'Text')\n\
+    -Y --status-item-title [title]        Set title of Status Item\n\
+    -L --status-item-icon [imagePath]     Set icon of Status Item\n\
+    -c --status-item-sysfont              Status Item should use the system font for menu item text\n\
+    -q --status-item-template-icon        Status Item icon should be treated as a template by AppKit\n\
 \n\
-    -f --bundled-file [filePath]       Add a bundled file or files (paths separated by \"|\")\n\
+    -f --bundled-file [filePath]          Add a bundled file or files (paths separated by \"|\")\n\
     \n\
-    -y --overwrite                     Overwrite any file/folder at destination path\n\
-    -d --symlink                       Symlink to script and bundled files instead of copying\n\
-    -l --optimize-nib                  Strip and compile bundled nib file to reduce size\n\
-    -E --nib-path [path]               Path to custom nib file\n\
-    -e --executable-path [path]        Path to custom ScriptExec binary\n\
+    -y --overwrite                        Overwrite any file/folder at destination path\n\
+    -d --symlink                          Symlink to script and bundled files instead of copying\n\
+    -l --optimize-nib                     Strip and compile bundled nib file to reduce size\n\
+    -E --nib-path [path]                  Path to custom nib file\n\
+    -e --executable-path [path]           Path to custom ScriptExec binary\n\
 \n\
-    -h --help                          Prints help\n\
-    -v --version                       Prints program name and version\n\
+    -h --help                             Prints help\n\
+    -v --version                          Prints program name and version\n\
 \n\
 See 'man platypus' or %@ for details.", PROGRAM_MANPAGE_URL);
 }
